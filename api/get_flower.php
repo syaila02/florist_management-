efd529465fb837ca9577a2392eb08f6ccc906848
syaila02 <syailzah02r@gmail.com>
2026-03-04 22:33:57 +0700

    add full CRUD API

---
+++ api/get_flower.php
@@ -0,0 +1,19 @@
+<?php
+header("Content-Type: application/json");
+include "../db.php";
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $query = "SELECT * FROM flowers WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+        echo json_encode($row);
+    } else {
+        echo json_encode(["message" => "Data tidak ditemukan"]);
+    }
+} else {
+    echo json_encode(["message" => "ID tidak diberikan"]);
+}
+?>
